a0d9477c44219ded7b256e623c3bcdd47958e899
NikitaLipatov <lipatov@tutu.ru>
2017-06-03 16:52:54 +0300

    MainActivity class and layout

---
+++ app/src/main/java/com/example/sony/remindme/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sony.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Nikita on 03.06.2017.
+ * Copyright © 2017 All rights reserved.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
